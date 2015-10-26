ccd843348825241c2f1af10cb19bc7bb6419ef13
Tran Thanh Nghia <tran.thanh.nghia@framgia.com>
2015-10-26 15:49:13 +0700

    data binding

--- app/src/main/java/com/nghiatt/demobinding/model/User.java
+++ app/src/main/java/com/nghiatt/demobinding/model/User.java
@@ -2,9 +2,24 @@ package com.nghiatt.demobinding.model;
 
 public class User {
   private String name;
+  private boolean isHappy;
 
   public User(String name) {
     this.name = name;
+    this.isHappy = true;
+  }
+
+  public User(String name, boolean isHappy) {
+    this.name = name;
+    this.isHappy = isHappy;
+  }
+
+  public boolean isHappy() {
+    return isHappy;
+  }
+
+  public void setIsHappy(boolean isHappy) {
+    this.isHappy = isHappy;
   }
 
   public String getName() {

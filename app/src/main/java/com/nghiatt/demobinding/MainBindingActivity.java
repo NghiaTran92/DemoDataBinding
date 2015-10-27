c208e3cf24916860a2b7a092a9ddc6159363ccf3
NghiaTran92 <thanhnghiaglhn92@gmail.com>
2015-10-28 02:10:35 +0700

    add example original

--- app/src/main/java/com/nghiatt/demobinding/MainBindingActivity.java
+++ app/src/main/java/com/nghiatt/demobinding/MainBindingActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
 import com.nghiatt.demobinding.databinding.MainBinding;
 import com.nghiatt.demobinding.model.User;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,31 +16,32 @@ import java.util.Date;
 *http://developer.android.com/intl/vi/tools/data-binding/guide.html
 * */
 public class MainBindingActivity extends AppCompatActivity {
-  private static final String TAG="MainBindingActivity";
-  private MainBinding binding;
-  private User user;
+    private static final String TAG = "MainBindingActivity";
+    private MainBinding binding;
+    private User user;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-    user = new User("User test text",false);
-    binding.setUser(user);
-    binding.setHandlers(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        user = new User("User test text", false);
+        binding.setUser(user);
+        binding.setHandlers(this);
+    }
 
-  public void onClickHappy(View view){
-    Log.e(TAG, "onClickHappy");
-    SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
-    user.setName("User unhappy at " + simpleDateFormat.format(new Date()));
-    binding.setUser(user);
-    user.setIsHappy(false);
-  }
+    public void onClickHappy(View view) {
+        Log.e(TAG, "onClickHappy");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        user.setName("User unhappy at " + simpleDateFormat.format(new Date()));
+        binding.setUser(user);
+        user.setIsHappy(false);
+    }
 
-  public void onClickUnHappy(View view) {
-    Log.e(TAG, "onClickUnHappy");
-    SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
-    user.setName("User happy at " + simpleDateFormat.format(new Date()));
-    binding.setUser(user);
-    user.setIsHappy(true);
-  }
+    public void onClickUnHappy(View view) {
+        Log.e(TAG, "onClickUnHappy");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        user.setName("User happy at " + simpleDateFormat.format(new Date()));
+        binding.setUser(user);
+        user.setIsHappy(true);
+    }
 }
--- app/src/main/java/com/nghiatt/demobinding/OriginActivity.java
+++ app/src/main/java/com/nghiatt/demobinding/OriginActivity.java
@@ -2,11 +2,62 @@ package com.nghiatt.demobinding;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class OriginActivity extends AppCompatActivity {
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_origin);
+import com.nghiatt.demobinding.model.User;
 
-  }
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OriginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "OriginActivity";
+
+    private Button mClick;
+    private TextView mTxtName;
+    private ImageView mImgFace;
+    private User mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_origin);
+
+        mUser = new User("User test text", false);
+        mClick = (Button) findViewById(R.id.btn_click);
+        mTxtName = (TextView) findViewById(R.id.txt_name);
+        mImgFace = (ImageView) findViewById(R.id.img_status);
+        mClick.setOnClickListener(this);
+        updateUser(mUser);
+    }
+
+    private void updateUser(User user) {
+        if (user != null) {
+            mTxtName.setText(user.getName());
+            mImgFace.setVisibility(user.isHappy() ? View.VISIBLE : View.GONE);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_click) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            if (mUser.isHappy()) {
+                Log.e(TAG, "onClickHappy");
+                mUser.setName("User unhappy at " + simpleDateFormat.format(new Date()));
+                mUser.setIsHappy(false);
+            } else {
+                Log.e(TAG, "onClickUnHappy");
+                mUser.setName("User happy at " + simpleDateFormat.format(new Date()));
+                mUser.setIsHappy(true);
+            }
+            updateUser(mUser);
+        }
+    }
 }
